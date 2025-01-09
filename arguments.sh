839127028b7d2a95e575d10805dd8f986bdfd12d
SatishPolaka <satishpolaka@gmail.com>
2025-01-09 18:30:53 +0530

    arg

---
+++ arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello A, How are you?"
+echo "$person1:: I am good, How about you?"
+echo "$person2:: I am good too."
+echo "$person1:: That's great to hear."
+echo "$person2:: Yes, it is."
+echo "$person1:: Bye $person2"
+echo "$person2:: Bye $person1"
