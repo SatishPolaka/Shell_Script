619aacf26b1f83e3ab403d7973acce485f0d880e
SatishPolaka <satishpolaka@gmail.com>
2025-01-09 18:40:51 +0530

    readcmd

---
+++ readcmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter username"
+
+read USERNAME
+
+echo "please enter password"
+
+read PASSWORD
+
+The above user name and password below
+echo "$USERNAME"
+echo "$PASSWORD"
