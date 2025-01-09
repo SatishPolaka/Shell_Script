11d3a41667a745ef28f6cc80272111f583d67867
SatishPolaka <satishpolaka@gmail.com>
2025-01-09 18:19:04 +0530

    var1

---
+++ variables01.sh
@@ -0,0 +1,13 @@
+#!/bib/bash
+
+person1=satish
+person2=gopi
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello A, How are you?"
+echo "$person1:: I am good, How about you?"
+echo "$person2:: I am good too."
+echo "$person1:: That's great to hear."
+echo "$person2:: Yes, it is."
+echo "$person1:: Bye $person2"
+echo "$person2:: Bye $person1"
