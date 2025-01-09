ee90423c8137cd24d02c59ad8de789b8b205fd74
SatishPolaka <satishpolaka@gmail.com>
2025-01-09 18:45:15 +0530

    hide

---
+++ readhide.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter username"
+
+read -s USERNAME
+
+echo "please enter password"
+
+read -s PASSWORD
+
+#The above user name and password below
+echo "$USERNAME"
+echo "$PASSWORD"
