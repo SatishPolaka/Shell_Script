34af5347224958f828ce5afe9c9b1c82461d6169
SatishPolaka <satishpolaka@gmail.com>
2025-01-09 18:06:47 +0530

    conservation

---
+++ Variables.sh
@@ -0,0 +1,10 @@
+#!/bib/bash
+
+echo "A:: Hi B"
+echo "B:: Hello A, How are you?"
+echo "A:: I am good, How about you?"
+echo "B:: I am good too."
+echo "A:: That's great to hear."
+echo "B:: Yes, it is."
+echo "A:: Bye B"
+echo "B:: Bye A"
