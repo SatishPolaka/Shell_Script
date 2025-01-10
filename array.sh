5551bc3e608e06fcbbee4aded5749dbc0094608a
SatishPolaka <satishpolaka@gmail.com>
2025-01-10 19:23:23 +0530

    addition

---
+++ array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+movies=("Aa" "Bb" "Cc" "Dd" "Ee")
+
+#index od array start from 0
+
+echo "movie is ${Movie[0]}"
+echo "movie is ${Movie[1]}" 
+echo "movie is ${Movie[2]}"
+echo "movie is ${Movie[3]}"
