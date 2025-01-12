e590eb6a6f464b2ddea37b40fbec2b0913882cab
SatishPolaka <satishpolaka@gmail.com>
2025-01-12 11:22:58 +0530

    addition

--- 01-install_script.sh
+++ 01-install_script.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "You must be the root user to excute the script"
+    exit 1
 fi
 
 dnf install mysql -y
--- spcl_var.sh
+++ spcl_var.sh
@@ -2,21 +2,21 @@
 
 #special variables
 
-echo "script name is $0"
-echo "first argument is $1"
-echo "second argument is $2"
-echo "all arguments are $@"
-echo "number of arguments are $#"
-echo "exit status of last command is $?"
-echo "process id of current shell is $$"
-echo "line number of current script is $LINENO"
-echo "home directory of current user is $HOME"
-echo "current working directory is $PWD"
-echo "user name is $USER"
-echo "hostname is $HOSTNAME"
-echo "random number is $RANDOM"
-echo "current bash version is $BASH_VERSION"
-echo "current bash pid is $BASHPID"
-sleep 10 &
+echo "script name is $0" # script name
+echo "first argument is $1" # first argument
+echo "second argument is $2" # and so on
+echo "all arguments are $@" # all arguments
+echo "number of arguments are $#" # number of arguments
+echo "exit status of last command is $?" # 0 means success, 1 means failure
+echo "process id of current shell is $$"   # process id of current shell
+echo "line number of current script is $LINENO"     # line number of current script
+echo "home directory of current user is $HOME" # home directory of current user
+echo "current working directory is $PWD" # current working directory
+echo "user name is $USER" # user name
+echo "hostname is $HOSTNAME" # hostname
+echo "random number is $RANDOM" # random number
+echo "current bash version is $BASH_VERSION" # current bash version
+echo "current bash pid is $BASHPID" # current bash pid
+sleep 10 & # sleep for 10 seconds
 
-echo "process id of last background command is $!"
+echo "process id of last background command is $!" # process id of last background command
