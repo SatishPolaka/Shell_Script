df032ac0bd26a2fb7edf26096221f0a20f6200dd
SatishPolaka <satishpolaka@gmail.com>
2025-01-13 16:05:16 +0530

    addition

--- 05-loops_installscrpit.sh
+++ 05-loops_installscrpit.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
 
-for i in {0..10}
-do 
-    echo $i
+# for i in {0..10}
+# do 
+#     echo $i
+# done
+
+USERID=$(id -u)
+
+# colors
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+#logs
+Log_Folder="/var/log/shell_script"
+Log_File=$( echo $0 | cut -d "." -f1 ) 
+TimeStamp=$(date "+%Y-%m-%d_%H-%M-%S")
+Log_File_Name="$Log_Folder/$Log_File-$TimeStamp.log"
+
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+    then
+        echo -e " $2 ... $R failed $N"
+        exit 1
+    else
+        echo -e "$2 ...  $G  sucess $N"            
+    fi
+
+}
+
+echo  -e " script started at $TimeStamp" >> $$Log_File_Name
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R  You must be the root user to excute the script $N "
+    exit 1
+fi
+
+for package in $@
+do
+        dnf list installed $package &>>$Log_File_Name
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>>$Log_File_Name
+        VALIDATE $? " installing $package"
+    else
+        echo -e " $Y $package is already installed $N "
+    fi
 done
